1a4e9b158cf2e7d668d78305570638891a16c262
William <William-Mac@Williamde-MacBook-Pro.local>
2016-12-01 06:03:41 +0800

    Initial commit

---
+++ Hello World.py
@@ -0,0 +1,13 @@
+print('Hello World')
+print(1 + 2)
+print(7*6)
+print()
+print("The End!")
+print("Python's strings are very easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+greeting = "Hello"
+name = " Bruce"
+print(greeting + name)
+
+print(greeting+'' + name)
