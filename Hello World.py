42d10f4cc7c2d9fa8b60dbc57d4e434b3551c720
William-Mac <william.wen@gmail.com>
2016-12-01 09:20:33 +0800

    Lecture 023. Understanding More About Python Section 05. The Basics of Python is finished.

--- Hello World.py
+++ Hello World.py
@@ -1,13 +1,25 @@
-print('Hello World')
-print(1 + 2)
-print(7*6)
-print()
-print("The End!")
-print("Python's strings are very easy to use")
-print('We can even include "quotes" in strings')
-print("Hello" + " world")
-greeting = "Hello"
-name = " Bruce"
-print(greeting + name)
+# greeting ="Hello"
+# name= input("Please enter your name ")
+# print (greeting+' '+name)
 
-print(greeting+'' + name)
+# splitString="This string has been \nsplit over\nseveral \nlines"
+# print (splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5"
+# print (tabbedString)
+
+print ('The pet shop owner said "No, no, \'e\'s un,....he\'s resting"')
+print ("The pet shop owner said \"No, no, 'e's un,....he's resting\"")
+
+anotherSplitString = """This is
+Waht you
+c
+a
+n
+
+see slipt
+string"""
+print (anotherSplitString)
+
+print ('''The pet shop owner said "No, no, 'e's uh..., he's resting"''')
+print ("""The pet shop owner said "No, no, 'e's uh..., he's resting" """)
